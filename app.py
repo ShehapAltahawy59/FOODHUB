c8ddfba8c063610815c690c45a4a7ccffc93c4c1
shehap62 <shehapkhalil62@gmail.com>
2024-10-09 22:42:55 +0300

    make changes

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import firebase_admin
 from flask_login import LoginManager, login_user,login_required, logout_user, current_user
 from firebase_admin import credentials, firestore, storage,auth
 from users.user import User
+import os
 # Initialize Flask app
 app = Flask(__name__)
 
@@ -14,7 +15,24 @@ login_manager.init_app(app)
 app.secret_key = '73ac6ddcd6a88955e0f720b20b459f93'
 
 # Initialize Firebase
-cred = credentials.Certificate('firebase_config.json')
+try:
+    cred = credentials.Certificate('firebase_config.json')
+except:
+    firebase_config = {
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),  # Ensure new lines are correct
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+    "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN"),
+    }
+    cred = credentials.Certificate(firebase_config)
+
+
 firebase_admin.initialize_app(cred, {
     'storageBucket': 'foodorderapp-2b2df.appspot.com'
 })
